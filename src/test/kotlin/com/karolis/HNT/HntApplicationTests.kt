7d86795fa00b118d6545c0435493af085ac3f472
karolis.beliasas <karolis.beliasas@mediapark.com>
2019-01-15 14:59:30 +0200

    Initial commit

---
+++ src/test/kotlin/com/karolis/HNT/HntApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.karolis.HNT
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class HntApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
